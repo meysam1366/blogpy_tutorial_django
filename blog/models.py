8213de5c9fd351ab6f2c4ca9de52e1d80edd8f57
Meysam Maghsoudi <safir.1987@gmail.com>
2020-04-14 02:21:17 +0430

    add models category and article and user profile

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User;
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
-# Create your models here.
+def validateFileExtention(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extentions = ['.jpg','.png']
+    if not ext.lower() in valid_extentions:
+        raise ValidationError('Unsupported file extention.')
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators = [validateFileExtention])
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=128,null=False,blank=False)
+    cover = models.FileField(upload_to='files/article_cover/', null=False, blank=False, validators = [validateFileExtention])
+    content = RichTextField()
+    created_at = models.DateTimeField(default = datetime.now, blank = False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE,default='')
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=128,null=False,blank=False,default='')
+    cover = models.FileField(upload_to='files/category_cover/', null = False, blank = False, validators = [validateFileExtention])
+
+    def __str__(self):
+        return self.title
